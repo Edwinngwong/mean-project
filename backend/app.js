67dde06b855c37b513262923b78e306a2d60bd4e
Edwin Ng <edwinngwong@gmail.com>
2022-01-05 16:51:21 -0500

    Fetched initial posts

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,25 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First middleware');
-    next();
-});
 
 
-app.use((req, res, next) => {
-    res.send('Hello from express');
+app.use('/api/posts' ,(req, res, next) => {
+    const posts = [
+        { 
+            id: '12345', 
+            title: 'first server-side post', 
+            content: 'this come from server'
+        },
+        { 
+            id: '12346', 
+            title: 'second server-side post', 
+            content: 'this come from server 2'
+        }
+    ];
+    res.status(200).json({
+        message: 'Posts fetched succesfully',
+        posts: posts
+    });
 });
 
 module.exports = app;
